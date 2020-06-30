9ee98600cfcfb6fd4f245c0fccbc1bfb5f17da19
Linus Strobel <lps116@ic.ac.uk>
2020-06-30 15:56:51 +0100

    recording schedule form complete

--- account/views.py
+++ account/views.py
@@ -1,17 +1,50 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from .forms import CreateRecordingForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from recordings.models import Recording, RadioStation
+from datetime import datetime
 
 # Create your views here.
-
+@login_required(login_url='/login/')
 def account_view(response, username):
+  if response.method == "POST":
+    form = CreateRecordingForm(response.POST)
+    if form.is_valid():
+      title = form.cleaned_data['title']
+      radio_station = form.cleaned_data['radio_station']
+      public = form.cleaned_data['public']
+      user = response.user
+      start_datetime = datetime.combine(form.cleaned_data['start_date'],
+                                        form.cleaned_data['start_time'])
+      end_datetime = datetime.combine(form.cleaned_data['end_date'],
+                                      form.cleaned_data['end_time'])
+      recording = Recording.objects.create(
+        user           = user,
+        title          = title,
+        radio_station  = radio_station,
+        public         = public,
+        start_datetime = start_datetime,
+        end_datetime   = end_datetime
+        )
+
+      recording.save()
+      messages.success(response, "Your recording has been scheduled")
+
+  form = CreateRecordingForm()
   user = User.objects.get(username=username)
-  scheduled_recordings = user.recordings.filter(status="pending")
-  completed_recordings = user.recordings.filter(status="complete")
-  inprogress_recordings = user.recordings.filter(status="in progress")
+  scheduled_recordings = user.recordings.filter(status="pending").order_by('start_datetime')
+  completed_recordings = user.recordings.filter(status="complete").order_by('-end_datetime')
+  inprogress_recordings = user.recordings.filter(status="in progress").order_by('start_datetime')
   context = {
-    "user" : user,
-    "scheduled_recordings": scheduled_recordings,
-    "completed_recordings" : completed_recordings,
-    "inprogress_recordings" : inprogress_recordings
-  }
-  return render(response, 'account/profile.html', context)
+        "user" : user,
+        "scheduled_recordings"  : scheduled_recordings,
+        "completed_recordings"  : completed_recordings,
+        "inprogress_recordings" : inprogress_recordings,
+        }
+  if user == response.user:
+    context["form"] = form
+    return render(response, 'account/profile.html', context)
+  else:
+    return render(response, 'account/profile.html', context)
