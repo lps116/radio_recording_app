7e763258c6b73a4a7a54f885a7c875973f918b5b
Linus Strobel <lps116@ic.ac.uk>
2020-07-03 13:58:30 +0100

    flash message styled

--- register/views.py
+++ register/views.py
@@ -10,7 +10,7 @@ def registration_view(response):
     form = RegistrationForm(response.POST)
     if form.is_valid():
       form.save()
-      messages.success(response, 'Thank you for signing up with us.')
+      messages.success(response, 'Thank you for signing up with us. Press Login to continue.')
       return redirect("/")
     else:
       messages.error(response, 'The form is invalid.')
