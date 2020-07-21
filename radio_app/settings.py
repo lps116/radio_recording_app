616b607ba639df7830ecfb930187c3fe04d695df
Linus Strobel <lps116@ic.ac.uk>
2020-07-21 10:49:33 +0100

    key works

--- radio_app/settings.py
+++ radio_app/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY ='3-timcn76gx#-0mechn8x_8w5e%*me$+f#&rytg#^#z49d2fha'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
