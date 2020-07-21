def15163d8db327ec23e1d3ebecc08c0f9d5dbb1
Linus Strobel <lps116@ic.ac.uk>
2020-07-21 10:17:23 +0100

    deploy

--- radio_app/settings.py
+++ radio_app/settings.py
@@ -34,7 +34,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['soundcapture.herokuapp.com/', '127.0.0.1']
+# ALLOWED_HOSTS = ['soundcapture.herokuapp.com/', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
