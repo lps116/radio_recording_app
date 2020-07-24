f64eed6ac6cbf6229523245ade65c4405c90b638
Linus Strobel <lps116@ic.ac.uk>
2020-07-24 15:07:03 +0100

    update

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
   path('', views.account_view, name='profile'),
   path('myrecordings/', views.recordings_view, name='myrecordings'),
   path('settings/', views.settings_view, name='settings'),
-  path('myrecordings/<int:recording_id>/', views.edit_view, name='myrecording'),
+  path('myrecordings/<int:recording_id>/edit/', views.edit_view, name='myrecording'),
   path('myrecordings/<int:recording_id>/delete/', views.delete_view, name='delete_recording'),
   path('myrecordings/create/', views.create_view, name='create_recording'),
+  path('myrecordings/<int:recording_id>/view/', views.listen_view, name='myrecording_listen')
 ]
